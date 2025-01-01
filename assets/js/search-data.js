e497206216dd35d9f2181f3a8caa9e00e0bfe213
vz415 <19888961+vz415@users.noreply.github.com>
2025-01-01 13:44:43 +0000

    Deploying to gh-pages from @ vz415/vzaballa@8271d3b5752ab2b24b51460276348220b62ffc12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/vzaballa/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/vzaballa/teaching/";
-          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
